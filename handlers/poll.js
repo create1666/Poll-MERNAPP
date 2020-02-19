f411e0eb9e2701835bb21b3f6b415348818119c2
OSAKINLE BOLUWAJI JOHN <create1666@gmail.com>
2020-02-19 01:29:25 +0100

    polls modified

--- handlers/poll.js
+++ handlers/poll.js
@@ -27,9 +27,9 @@ exports.createPoll = async (req, res, next)=>{
           votes: 0}))
         
           });
-        /* user.polls.push(poll._id);
-         await user.save();*/
-          res.status(201).json({...poll._doc});
+         user.polls.push(poll._id);
+         await user.save();
+          res.status(201).json({...poll._doc, user: user._id});
     } catch (err){
         err.status = 400;
         next(err);
